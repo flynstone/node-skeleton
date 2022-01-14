d2644bb8169d95f427a2bbf02938722fa07c0429
Akremibrahim <akrem.ibrahim@gmail.comgit config --global user.name Akrem>
2022-01-14 02:27:33 +0000

    login js. and logout

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,2 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (email, password) VALUES ('fake@user.com', 'password');
+INSERT INTO users (email, password) VALUES ('test@user.com', 'password');
